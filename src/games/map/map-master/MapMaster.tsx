d33f5f07dd1e93e99ce000f37d4a6b469e2864f1
Bara E. Salameh <barasalameh90@gmail.com>
2025-09-19 18:42:04 +0300

    Initial game branch commit

---
+++ src/games/map/map-master/MapMaster.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Counter, Map } from "@/components/shared";
+// import { Starter } from "./Starter";
+// import { Hint } from "./Hint";
+// import { WinLose } from "./WinLose";
+// import { useDetermineCounter, useGenerateRandomCountry } from "./hooks";
+
+export const MapMaster = () => {
+    // useGenerateRandomCountry();
+    // useDetermineCounter();
+      
+    return (
+        <div className="h-screen">
+            <div className="fixed h-screen left-5 flex items-center z-1000">
+                {/* <Starter />
+                <Hint />
+                <WinLose /> */}
+                <Counter />
+            </div>
+            <Map mode="map" />
+        </div>
+    )
+}
---
+++ src/games/map/map-master/index.ts
@@ -0,0 +1,1 @@
+export { MapMaster } from './MapMaster';
