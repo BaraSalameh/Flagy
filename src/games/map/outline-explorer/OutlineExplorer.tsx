9c739b19361916d4e73d91514b2823a3bbc0b80f
Bara E. Salameh <barasalameh90@gmail.com>
2025-10-02 16:16:21 +0300

    Updating Outline Explorer to be more generic

--- src/games/map/outline-explorer/OutlineExplorer.tsx
+++ src/games/map/outline-explorer/OutlineExplorer.tsx
@@ -1,29 +1,56 @@
 'use client';
 
 import { Counter, CountryMenu, Map, ProgressTracker } from "@/components/shared";
-import { Starter } from "./Starter";
-import { useDetermineCounter, useDetermineOutlineExplorerCounter, useGenerateRandomCountry } from "./hooks";
+import { useDetermineOutlineExplorerCounter, useGenerateRandomCountry } from "./hooks";
 import { useAppSelector } from "@/lib/store/hooks";
-import { setCurrentCountry } from "@/lib/store/slices/outlineExplorerSlice";
+import { clearOutlineExplorer, setCurrentCountry } from "@/lib/store/slices/outlineExplorerSlice";
 import { Text } from "@/components/ui";
-import { WinLose } from "./WinLose";
+import { GameStarterModal } from "../shared/components/GameStarterModal";
+import { useGeneralCounter } from "../shared/hooks/useGeneralCounter";
+import { GameOverModal } from "../shared/components/GameOverModal";
 
 export const OutlineExplorer = () => {
-    useGenerateRandomCountry();
-    useDetermineCounter();
-    useDetermineOutlineExplorerCounter();
+    const generalState = useAppSelector(state => state.general);
+    const generalCounter = generalState.counter;
 
     const outlineExplorerState = useAppSelector(state => state.outlineExplorer);
-    const randomCountries =outlineExplorerState.randomCountries;
-    const counter = outlineExplorerState.counter;
+    const currentCountry = outlineExplorerState.currentCountry;
+    const randomCountries = outlineExplorerState.randomCountries;
+    const outlineExplorerCounter = outlineExplorerState.counter;
+
+    useGenerateRandomCountry();
+    useDetermineOutlineExplorerCounter();
+    useGeneralCounter(currentCountry);
 
     return (
         <div className="relative h-screen w-screen">
             <Map game="outline-explorer" />
-            <Starter />
+            <GameStarterModal
+                title="Outline Explorer"
+                description={`Test your geography skills in Outline Explorer! Each round, you’ll see the outline of a mystery country. Can you guess its name before time runs out?`}
+            />
             <Counter />
-            <WinLose />
-            <ProgressTracker counter={counter} maxCounter={20} content={<Text position='center' size='sm'>Mystery country?</Text>} />
+            <GameOverModal
+                thresholds={[
+                    {
+                        condition: (outlineExplorerCounter <= 0),
+                        result: false,
+                        message: `😅 Work harder!`
+                    },
+                    {
+                        condition: (outlineExplorerCounter >= 20),
+                        result: true,
+                        message: `🚀 Good job! Keep it up!`
+                    },
+                    {
+                        condition: (generalCounter === 20),
+                        result: false,
+                        message: `😅 You need to be smarter!`
+                    }
+                ]}
+                onClear={clearOutlineExplorer}
+            />
+            <ProgressTracker counter={outlineExplorerCounter} maxCounter={20} content={<Text position='center' size='sm'>Mystery country?</Text>} />
             <CountryMenu
                 randomCountries={randomCountries}
                 onAction={setCurrentCountry}
--- src/games/map/outline-explorer/hooks/index.ts
+++ src/games/map/outline-explorer/hooks/index.ts
@@ -1,3 +1,2 @@
 export { useGenerateRandomCountry} from './useGenerateRandomCountry';
 export { useDetermineOutlineExplorerCounter } from './useDetermineOutlineExplorerCounter';
-export { useDetermineCounter } from './useDetermineCounter';
